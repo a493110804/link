4ec5b4c18dd7292ddd79a341906f16acaa874744
a493110804 <493110804@qq.com>
2015-12-02 20:43:20 +0800

    Initial Commit

---
+++ link/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  link
+//
+//  Created by 魏新宇 on 15/12/2.
+//  Copyright © 2015年 魏新宇. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
